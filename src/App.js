8f1f3e3940249596471b16ab1416caea2a54fd49
acapasso <69538713+acapasso90@users.noreply.github.com>
2020-11-29 13:13:13 -0500

    Created KK Slider React App. Added KK Slider image without background.
    Created KK Slider React App. Added KK Slider image without background to media folder and Snowy KK. Began SearchSong component and linked into app.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import SearchSong from "./SearchSong.js";
 
 function App() {
-  return (
+  return ( <div className="Container">
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <SearchSong />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      </div>
     </div>
   );
 }
---
+++ src/SearchSong.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function SearchSong(){ return(<div class="searchSong">
+<div class="container">
+
+</div>
+</div>);}
