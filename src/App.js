26c80b7f6f72d1a71a5e0e8ec15fb8ba2b757d3b
acapasso <69538713+acapasso90@users.noreply.github.com>
2020-12-01 15:20:36 -0500

    Added background image, centered app in CSS. Installed and updated font on .app
    Added background image, centered app in CSS. Installed and updated font on .app

--- src/App.js
+++ src/App.js
@@ -4,13 +4,8 @@ import SearchSong from "./SearchSong.js";
 function App() {
   return ( <div className="container">
     <div className="app">
-      <header className="app-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-      </div>
       <SearchSong  />
+      </div>
       </div>
   );
 }
