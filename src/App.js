6f13d9c65ec7faef303be7e780ea78aae1bc6b3c
acapasso <69538713+acapasso90@users.noreply.github.com>
2020-12-04 09:07:59 -0500

    Updated mobile SerachSong to be in a Small Div rather than seperate component
    Updated mobile SerachSong to be in a Small Div rather than seperate component. Adjusted mobile header backdrop to be smaller. Changed boxshadow color on Header from white to gray

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import './App.css';
 import Header from "./Header.js";
 import SmallHeader from "./SmallHeader.js"
 import SearchSong from "./SearchSong.js";
-import SmallSearchSong from "./SmallSearchSong.js"
 import MediaQuery from 'react-responsive'
 
 function App() {
   return ( 
     <div className="app">
         <MediaQuery maxDeviceWidth={500}><SmallHeader />
-        <SmallSearchSong /> </MediaQuery>
+       <div className="smallSearchSong"><SearchSong /></div>  </MediaQuery>
         <MediaQuery minDeviceWidth={501}> <Header />  
         <SearchSong  />     </MediaQuery> 
       </div>
