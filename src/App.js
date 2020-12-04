f1d7140c96a4f331ccee3c25f397c71b43b16b44
acapasso <69538713+acapasso90@users.noreply.github.com>
2020-12-04 10:32:42 -0500

    Put SmallHeader into a div instead of seperate component
    Put SmallHeader into a div instead of seperate component, restyled so it fits mobile devices

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
 import Header from "./Header.js";
-import SmallHeader from "./SmallHeader.js"
 import SearchSong from "./SearchSong.js";
 import MediaQuery from 'react-responsive'
 
 function App() {
   return ( 
     <div className="app">
-        <MediaQuery maxDeviceWidth={500}><SmallHeader />
+        <MediaQuery maxDeviceWidth={500}><div className="smallHeader"><Header /></div>
        <div className="smallSearchSong"><SearchSong /></div>  </MediaQuery>
         <MediaQuery minDeviceWidth={501}> <Header />  
         <SearchSong  />     </MediaQuery> 
