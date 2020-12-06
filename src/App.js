9a06e15ca44b38d27484484e2fef3ad3d5523c92
acapasso <69538713+acapasso90@users.noreply.github.com>
2020-12-06 11:11:21 -0500

    Styled tablet header, began styling tablet searchsong. Moved tablet footer on top
    Styled tablet header, began styling tablet searchsong. Moved tablet footer on top

--- src/App.js
+++ src/App.js
@@ -8,10 +8,16 @@ function App() {
     <div className="app">
         <MediaQuery maxDeviceWidth={500}><div className="smallHeader"><Header /></div>
        <div className="smallSearchSong"><SearchSong /></div>  </MediaQuery>
-        <MediaQuery minDeviceWidth={501}><div className="tabletHeader"> <Header />  </div> 
-        <SearchSong  />     </MediaQuery> 
-        <MediaQuery minDeviceWidth={1025}><Header />  
-        <SearchSong  />     </MediaQuery> 
+        <MediaQuery maxDeviceWidth={1025}><div className="tabletHeader"> <Header />  </div> 
+        <div className="tabletFooter">         <footer>
+        <div className="credit">
+      <p>  All images, songs, and characters found on the app are the sole property of Nintendo and Animal Crossing <br />
+       and are only used for non-commercial and educational purpose.</p>
+      </div>
+      Coded by <a href="https://www.amandacapasso.com">Amanda Capasso</a>
+        </footer></div> <div className="tabletSearchSong"><SearchSong  />  </div>   </MediaQuery> 
+        <MediaQuery minDeviceWidth={1026}><Header />  
+        <SearchSong  />    
         <footer>
         <div className="credit">
       <p>  All images, songs, and characters found on the app are the sole property of Nintendo and Animal Crossing <br />
@@ -19,6 +25,7 @@ function App() {
       </div>
       Coded by <a href="https://www.amandacapasso.com">Amanda Capasso</a>
         </footer>
+        </MediaQuery> 
       </div>
   );
 }
