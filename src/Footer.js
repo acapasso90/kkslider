d98589ef2d8c33b8422b191f723c00307d4d7bcf
acapasso <69538713+acapasso90@users.noreply.github.com>
2021-02-18 17:54:55 -0500

    removed footer backdrop replaced with .footer with background color
    removed footer backdrop replaced with .footer with semi-transparent background color to prevent backdrop from shifting on mobile

--- src/Footer.js
+++ src/Footer.js
@@ -3,12 +3,13 @@ import React from "react";
 export default function Footer(){
     return(<div className="Footer">
         <footer>
-        <div className="backdrop"></div>
+        <div className="footer">
         <div className="credit">
       <p>  All images, songs, and characters found on the app are the sole property of Nintendo and Animal Crossing <br />
        and are only used for non-commercial and educational purpose.</p>
       </div>
       <a href="https://github.com/acapasso90/kkslider" target="blank" rel="noreferrer">Open-source app </a> coded by <a href="https://www.amandacapasso.com"  target="blank" rel="noreferrer">Amanda Capasso</a> using the <a href="http://acnhapi.com/" target="blank" rel="noreferrer">ACNH API</a>
+      </div>
         </footer>
         </div>
     )
